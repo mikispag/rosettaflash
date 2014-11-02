0b939a603bc954b445728c4e687e2a88d32043a7
Michele Spagnuolo <mikispag@gmail.com>
2014-11-02 12:47:32 +0100

    Added license headers.

--- rosettaflash.go
+++ rosettaflash.go
@@ -1,10 +1,26 @@
 /*
+ * Copyright 2014 Google Inc. All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * 
  * Rosetta Flash - a tool for manipulating SWF files.
  *
  * More info at:
- * http://miki.it/blog/2014/7/8/abusing-jsonp-with-rosetta-flash/
+ * https://miki.it/blog/2014/7/8/abusing-jsonp-with-rosetta-flash/
  *
- * Michele Spagnuolo - http://miki.it
+ * Michele Spagnuolo - https://miki.it
  * 8 July 2014
  */
 
